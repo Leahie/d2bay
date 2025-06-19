d90befca68d597c331dffeb46b5e3f4ce41a48c0
Leah Zhang <LeahZhang1595995@gmail.com>
2025-06-19 19:50:30 -0400

    making my way

--- soc_media_project/users/urls.py
+++ soc_media_project/users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('tab/', views.tab, name='tab'),
+    path('users/', views.users, name='users'),
+    path('users/details/<int:id>', views.details, name='details')
 ]
--- soc_media_project/users/views.py
+++ soc_media_project/users/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader 
+from .models import User
 
 # Create your views here.
 def tab(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
+
+def users(request):
+    myusers = User.objects.all().values()
+    template = loader.get_template('allusers.html')
+    context = {
+        'myusers': myusers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    myuser = User.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {'user':myuser}
+    return HttpResponse(template.render(context, request))
